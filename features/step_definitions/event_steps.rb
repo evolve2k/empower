69c644dae8dedf972375ecb08921ef832e097fba
Jonathan Clarke <jonathan@beilabs.com>
1999-12-18 13:09:29 +1100

    Adding blueprint for event

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -1,16 +1,9 @@
-Given /^I am logged in as "([^\"]*)"$/ do |email|
-  Volunteer.make(:email => email)
-  visit '/volunteer-login'
-  fill_in 'email', :with => email
-  fill_in 'password', :with => 'm00c0w'
-  click_button 'Login'
-end
-
 Given /^there are no events available$/ do
   @events = nil
 end
 
-Given /^there are events available$/ do
-  @events = Event.find :all
+Given /^there are many events available$/ do
+  (1..10).each do |i|
+    Event.make(:event_date => i.weeks.ago)
+  end
 end
-
