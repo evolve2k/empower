fda41c19a826ccbae06a1187093888b9c256a2b0
Zubin Henner <zubin@zenlunacy.com>
2010-04-17 21:11:58 +1000

    Admin required warning.

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,8 +1,13 @@
 class Admin::BadgesController < ResourceController::Base
-  # before_filter :admin_required
-  before_filter { flash[:error] = "TODO: admin_required" }
+  before_filter :admin_required
   layout 'admin'
   
   show.wants.html  { redirect_to admin_badges_path }
   index.wants.html { redirect_to(new_admin_badge_path) if Badge.count.zero? }
+  
+private
+  
+  def admin_required
+    flash[:error] = "TODO: admin_required"
+  end
 end
