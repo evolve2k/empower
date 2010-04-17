6bc7b25699b65300c5491475bd8f3501a056cc28
Xavier Shay <xavier@rhnh.net>
2010-04-17 19:48:21 +1000

    Block the extra who inputs. Wish I didn't need an extra DIV, not sure how to avoid.

--- app/coffee/events/new.coffee
+++ app/coffee/events/new.coffee
@@ -3,9 +3,9 @@ $(document).ready ->
     lastInput: $(this).parent().find('input').last()
 
     if (lastInput.val().length > 0)
-      $(this).parent().append("<input class='who-input' />");
-      console.log("add new")
+      $(this).parent().append("<br /><input type='text' class='who-input' placeholder='Leave blank if no more'/>");
     else
-      secondLast: lastInput.prev('input')
+      secondLast: lastInput.prevAll('input').first()
       if (secondLast.size() == 1 && secondLast.val().length == 0)
+        lastInput.prev('br').remove()
         lastInput.remove()
--- public/javascripts/events/new.js
+++ public/javascripts/events/new.js
@@ -4,11 +4,13 @@
       var lastInput, secondLast;
       lastInput = $(this).parent().find('input').last();
       if ((lastInput.val().length > 0)) {
-        $(this).parent().append("<input class='who-input' />");
-        return console.log("add new");
+        return $(this).parent().append("<br /><input type='text' class='who-input' placeholder='Leave blank if no more'/>");
       } else {
-        secondLast = lastInput.prev('input');
-        return (secondLast.size() === 1 && secondLast.val().length === 0) ? lastInput.remove() : null;
+        secondLast = lastInput.prevAll('input').first();
+        if ((secondLast.size() === 1 && secondLast.val().length === 0)) {
+          lastInput.prev('br').remove();
+          return lastInput.remove();
+        }
       }
     });
   });
