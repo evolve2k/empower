ef00e8e5412af53f932367c0c8f3f668cdfb3150
Alan Harper <alan@aussiegeek.net>
2010-04-17 17:28:38 +1000

    Add blueprint

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@
 # from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'blueprint.rb'))
+
 require 'spec/autorun'
 require 'spec/rails'
 
