275c90b5319cb7c6d38255d7a2b03e7b0b7779e8
Jonathan Clarke <jonathan@beilabs.com>
2010-04-18 01:55:07 +1000

    Basic crud for events

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -6,9 +6,14 @@ class Admin::EventsController < AdminController
 
   def new
     @event = Event.new
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @volunteer }
+    end
   end
 
   def create
+    @event = Event.new(params[:event])
     begin
       @event.save!
       flash[:notice] = 'Event was successfully created.'
@@ -20,15 +25,23 @@ class Admin::EventsController < AdminController
   end
 
   def show
-    @event = Event.find(params[:id])
+    @event = Event.find_by_permalink(params[:id])
   end
 
   def edit
-    @event = Event.find(params[:id])
+    @event = Event.find_by_permalink(params[:id])
   end
 
   def update
-    @event = Event.find(params[:id])
+    @event = Event.find_by_permalink(params[:id])
+
+    if @event.update_attributes(params[:event])
+      flash[:notice] = 'Event was successfully updated.'
+      redirect_to admin_event_path(@event)
+    else
+      flash[:error] = "There was an issue saving the event."
+      render :action => "edit"
+    end
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,22 @@
 class Event < ActiveRecord::Base
+  belongs_to :location
+  has_one :volunteer, :through => :creator_id
+  validates_presence_of :name
+  validates_presence_of :location_id
+  validates_presence_of :creator_id
+  validates_presence_of :event_date
+  validates_uniqueness_of :permalink
+
+  before_update :set_permalink
+  before_save :set_permalink
+
+  def set_permalink
+    self.permalink = (name.downcase.gsub(/[^a-z0-9]+/i, '-'))
+  end
+
+  def to_param
+    permalink
+  end
+
+
 end
