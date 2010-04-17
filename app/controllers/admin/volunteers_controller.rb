6a6033bc352926dde7b1e7f442d6ed0f638c9e8d
Scott Harvey <scott@wickedweasel.com>
2010-04-17 12:22:45 +0000

    stuff

--- app/controllers/admin/volunteers_controller.rb
+++ app/controllers/admin/volunteers_controller.rb
@@ -1,18 +1,6 @@
 class Admin::VolunteersController < AdminController
+  resource_controller
+  
   layout "admin"
-
-  def index
-    @volunteers = Volunteer.all
-  end
-
-  def new
-    @volunteer = Volunteer.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @volunteer }
-    end
-  end
-
 end
 
