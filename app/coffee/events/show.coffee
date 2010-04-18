5b047764463f0b6493e468f79717819cbdb3cb5c
Xavier Shay <xavier@rhnh.net>
2010-04-18 13:56:40 +1000

    Calculate totals

--- app/coffee/events/show.coffee
+++ app/coffee/events/show.coffee
@@ -1,2 +1,11 @@
 $(document).ready ->
   $('.tableForm').formRowBuilder('sale-input')
+
+  recalculateTotals: () ->
+    totalSales: $('.sale-amount').inject(0, (acc) -> acc + $(this).val() * 1)
+    totalCash: totalSales - $('#float-amount').val() * 1
+
+    $('#total-sales').text("\$$totalSales")
+    $('#total-cash').text("\$$totalCash")
+  $('.sale-amount').live 'keyup', () ->
+    recalculateTotals()
--- public/javascripts/events/show.js
+++ public/javascripts/events/show.js
@@ -1,5 +1,18 @@
 (function(){
   $(document).ready(function() {
-    return $('.tableForm').formRowBuilder('sale-input');
+    var recalculateTotals;
+    $('.tableForm').formRowBuilder('sale-input');
+    recalculateTotals = function recalculateTotals() {
+      var totalCash, totalSales;
+      totalSales = $('.sale-amount').inject(0, function(acc) {
+        return acc + $(this).val() * 1;
+      });
+      totalCash = totalSales - $('#float-amount').val() * 1;
+      $('#total-sales').text("\$" + totalSales);
+      return $('#total-cash').text("\$" + totalCash);
+    };
+    return $('.sale-amount').live('keyup', function() {
+      return recalculateTotals();
+    });
   });
 })();
