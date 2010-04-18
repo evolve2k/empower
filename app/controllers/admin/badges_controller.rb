34809253d3eb2a603fc5a241672883acfed9fec1
Cameron Walsh <cameron.walsh@gmail.com>
2010-04-18 21:04:06 +1000

    Fixed badges controller logging in.

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,5 +1,6 @@
 class Admin::BadgesController < ResourceController::Base
   before_filter :admin_required
+  before_filter :require_volunteer
   layout 'admin'
   
   show.wants.html  { redirect_to admin_badges_path }
@@ -15,5 +16,27 @@ private
   
   def set_active_tab
     @active_tab = "badges"
+  end
+
+  def require_volunteer
+    unless current_volunteer
+      redirect_to volunteer_login_path
+      return false
+    end
+  end
+
+  def current_volunteer_session
+    return @current_volunteer_session if defined?(@current_volunteer_session)
+    @current_volunteer_session = VolunteerSession.find
+  end
+
+  helper_method :logged_in?
+  def logged_in?
+    current_volunteer
+  end
+
+  def current_volunteer
+    return @current_volunteer if defined?(@current_volunteer)
+    @current_volunteer = current_volunteer_session && current_volunteer_session.record
   end
 end
