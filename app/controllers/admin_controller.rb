926f8f6a54dfb436603bd3b148d5c9d5887d5463
Xavier Shay <xavier@rhnh.net>
2010-04-18 16:03:19 +1000

    Sketching out volunteer dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,8 @@
 class AdminController < ApplicationController
   layout 'admin'
   before_filter :require_volunteer
-  
+
   def index
-    
+
   end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,19 @@
 # This is a mock controller for designing event wrapup forms. Please don't
 # delete coz I'm still working on them.
 class EventsController < ApplicationController
+  class Event < Struct.new(:title, :sales, :day, :volunteers); end;
+
   def new
   end
 
   def show
   end
-  
-  private
-  def set_active_tab
-    @active_tab = "events"
+
+  def volunteer
+    @events = [
+      Event.new("Bondi Market", 1500, Date.today, %w(xavier xavier-2)),
+      Event.new("Rose St Artist Market", 950, Date.yesterday, %w(xavier-2)),
+      Event.new("Paddington Market", 2310, Date.today - 7.days, %w(chris xavier-3 xavier))
+    ]
   end
 end
