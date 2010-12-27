73a3ce7f80d9b0ed518c7ed21bf66d0d9def264f
Richie Khoo <evolve2k@gmail.com>
2010-12-27 22:16:59 +1100

    Rails upgrade plugin generated routes file saved to routes_new.rb

---
+++ config/routes_new.rb
@@ -0,0 +1,33 @@
+(in /Users/richie/Projects/empower)
+Empower::Application.routes.draw do
+  resource :volunteer_sessions
+  resources :events do
+    collection do
+  get :volunteer
+  end
+  
+  
+  end
+
+  resources :volunteers
+  match '/volunteer-login' => 'volunteer_sessions#new', :as => :volunteer_login
+  namespace :admin do
+      match '/volunteer-logout' => 'volunteer_sessions#destroy', :as => :volunteer_logout
+      match '/admin' => 'admin#index', :as => :admin
+      resources :events
+      resources :volunteers
+      resources :badges
+      resources :roles
+      resources :locations
+      resources :regions
+      resource :dashboard do
+    
+        member do
+    get :volunteer
+    end
+    
+    end
+  end
+
+  match '/' => 'admin#index'
+end
