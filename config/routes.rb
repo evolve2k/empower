24c4e7b71ebf3068177c75f5b313cfb6bde29ea9
Richie Khoo <evolve2k@gmail.com>
2011-08-23 22:45:04 +1000

    Tidy routes file following upgrade + updated bundle config file

--- config/routes.rb
+++ config/routes.rb
@@ -2,31 +2,25 @@ Empower::Application.routes.draw do
   resource :volunteer_sessions
   resources :events do
     collection do
-  get :volunteer
+      get :volunteer
+    end
   end
-  
-  
-  end
-
   resources :volunteers
   match '/volunteer-login' => 'volunteer_sessions#new', :as => :volunteer_login
   namespace :admin do
-      match '/volunteer-logout' => 'volunteer_sessions#destroy', :as => :volunteer_logout
-      match '/admin' => 'admin#index', :as => :admin
-      resources :events
-      resources :volunteers
-      resources :badges
-      resources :roles
-      resources :locations
-      resources :regions
-      resource :dashboard do
-    
-        member do
-    get :volunteer
-    end
-    
+    match '/volunteer-logout' => 'volunteer_sessions#destroy', :as => :volunteer_logout
+    match '/admin' => 'admin#index', :as => :admin
+    resources :events
+    resources :volunteers
+    resources :badges
+    resources :roles
+    resources :locations
+    resources :regions
+    resource :dashboard do
+      member do
+        get :volunteer
+      end
     end
   end
-
   match '/' => 'admin#index'
 end
